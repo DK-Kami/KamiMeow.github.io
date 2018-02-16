eb33160acc4d0c26b1ef7080de5d0dcd2b604372
Kami-san <34164816+KamiMeow@users.noreply.github.com>
2018-02-16 11:22:17 +0300

    cafe

---
+++ cafe-later/js/main.js
@@ -0,0 +1,25 @@
+function Hide1() {
+    document.getElementById("Prof1").className = "bioC"
+}
+function Hide2() {
+    document.getElementById("Prof2").className = "bioC"
+}
+function Hide3() {
+    document.getElementById("Prof3").className = "bioC"
+}
+function Hide4() {
+    document.getElementById("Prof4").className = "bioC"
+}
+
+function Show1() {
+    document.getElementById("Prof1").className = "bio"
+}
+function Show2() {
+    document.getElementById("Prof2").className = "bio"
+}
+function Show3() {
+    document.getElementById("Prof3").className = "bio"
+}
+function Show4() {
+    document.getElementById("Prof4").className = "bio"
+}
