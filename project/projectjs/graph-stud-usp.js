9185897d56523c69d1abdaa03a17a7c3525f4a51
Kami-san <34164816+KamiMeow@users.noreply.github.com>
2018-09-02 12:49:00 +0300

    Robo Dance

--- project/projectjs/graph-stud-usp.js
+++ project/projectjs/graph-stud-usp.js
@@ -1,12 +1,5 @@
-var row1 = document.getElementById("row1");
-var row2 = document.getElementById("row2");
+var year = document.getElementById("year");
+var semestr = document.getElementById("semestr");
 
-row1.addEventListener("click", function() {
-    row1.classList = "row-nav active-row";
-    row2.classList = "row-nav";
-    console.log("1");
-})
-row2.addEventListener("click", function() {
-    row2.classList = "row-nav active-row";
-    row1.classList = "row-nav";
-})
+var yearArr = year.childNodes;
+console.log(yearArr);
