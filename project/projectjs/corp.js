366aae39514ef287a656c8bed3d3e7655820a82a
Kami-san <34164816+KamiMeow@users.noreply.github.com>
2018-09-02 19:46:07 +0300

    Robo Dance

--- project/projectjs/corp.js
+++ project/projectjs/corp.js
@@ -1,5 +1,14 @@
-var text = document.getElementById("sent")
+var text = document.getElementById("sent");
+var corp = document.getElementById("list");
+var corpData = document.getElementById("corp-data");
+var corpSearch = document.getElementById("corp-search");
 
 function sent(value) {
     text.textContent = value;
+}
+
+function openDown(id) {
+    corp.style.display = "none";
+    corpSearch.style.display = "none";
+    corpData.childNodes[id].style.display = "block";
 }
